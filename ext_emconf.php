b854359a67abeb6bec07f19ecaaec0ac61015aa7
Łukasz Uznański <lukaszuznanski94@gmail.com>
2023-03-14 12:15:08 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,20 +3,20 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Headless Solr',
     'description' => 'This extension provides integration with solr to output content from TYPO3 in JSON format.',
-    'state' => 'beta',
+    'state' => 'stable',
     'author' => 'Łukasz Uznański',
     'author_email' => 'extensions@macopedia.com',
     'category' => 'fe',
     'internal' => '',
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.0.99',
-            'frontend' => '9.5.0-10.0.99'
+            'typo3' => '10.0.99-11.9.99',
+            'frontend' => '10.0.99-11.9.99'
         ],
         'conflicts' => [],
         'suggests' => [
-            'headless' => '2.0.0-2.9.9'
+            'headless' => '2.0.0-4.9.9'
         ]
     ],
 ];
